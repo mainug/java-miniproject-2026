9ec4ff0b29283357d65e890420ddb109174c7891
mainug <alsdnr0850@gmail.com>
2026-04-29 16:54:48 +0900

    주석 수정

--- usedtrade/src/main/java/com/pknu26/usedtrade/dto/PostDTO.java
+++ usedtrade/src/main/java/com/pknu26/usedtrade/dto/PostDTO.java
@@ -7,7 +7,7 @@ public class PostDTO {
 
     private Long postId;       // DB의 post_id (PK)
     private Long userId;       // 작성자 ID (FK)
-    private Long boardId;     // 카테고리/게시판 ID (FK)
+    private Long boardId;      // 카테고리/게시판 ID (FK)
     private String title;      // 제목
     private String content;    // 내용
     private Long price;        // 가격
