3a143d099362ad53731d62d3d9b9cb011c3d2bc4
mainug <alsdnr0850@gmail.com>
2026-04-30 14:46:25 +0900

    테스트 커밋

--- usedtrade/src/main/java/com/pknu26/usedtrade/config/WebConfig.java
+++ usedtrade/src/main/java/com/pknu26/usedtrade/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// 웹 설정
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
